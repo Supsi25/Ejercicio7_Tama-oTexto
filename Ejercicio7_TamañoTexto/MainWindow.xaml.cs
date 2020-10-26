1396225a25290f3ecaac8e32a6ffaf6cdac7357d
susana <susana.casas@iesdoctorbalmis.com>
2020-10-26 16:09:19 +0100

    Agregar archivos de proyecto.

---
+++ Ejercicio7_TamañoTexto/MainWindow.xaml.cs
@@ -0,0 +1,21 @@
+﻿using System.Windows;
+using System.Windows.Controls;
+
+namespace Ejercicio7_TamañoTexto
+{
+    /// <summary>
+    /// Lógica de interacción para MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Boton_Button_Checked(object sender, RoutedEventArgs e)
+        {
+            texto_TextBlock.FontSize = int.Parse((sender as RadioButton).Tag.ToString());
+        }
+    }
+}
